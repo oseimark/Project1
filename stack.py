c5657d865b4d3bb171241d647e0841db10c7d696
krystal <oseimarkfinley@outlook.com>
2023-03-07 23:27:09 +0000

    Stack python added

---
+++ stack.py
@@ -0,0 +1,15 @@
+#push
+list =[]
+list.append(int(input("Enter a number to push into the stack: ")))
+print("push:",list)
+list.append(int(input("Enter a number to push into the stack: ")))
+print("push:",list)
+list.append(int(input("Enter a number to push into the stack: ")))
+print("push:",list)
+
+#pop
+list.pop()
+print("pop: ",list)
+
+#peek
+print("peek:",list[-1])
